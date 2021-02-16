495ba3a7365af50aa0e5f591aa024f769b405cc2
Aniruddha Bagal <bagalaniruddha@gmail.com>
2021-02-16 12:05:41 +0000

    Took input of two numbers and swapped the values

--- Program_5.c
+++ Program_5.c
@@ -14,14 +14,14 @@ int main()
 
     //Write your code here
 
-    printf("The Value of a and b before swapping is %d %d\n", a, b);
+    printf(" The Value of a and b before swapping is %d %d\n", a, b);
     
     temp = a;
     a = b;
     b = temp;
 
 
-    printf("The Value of a and b after swapping is %d %d\n", a, b);
+    printf(" The Value of a and b after swapping is %d %d\n", a, b);
     
     return 0;
 }
